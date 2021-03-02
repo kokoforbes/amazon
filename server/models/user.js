75ccaabf93525e60f10fb265afe0ce0578432b7a
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-03-02 14:37:54 +0100

    Created user schema and connecttion to DB

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: String,
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  address: { type: Schema.Types.ObjectId, ref: 'Address' },
+});
+
+module.exports = mongoose.model('user', UserSchema);
--- server/server.js
+++ server/server.js
@@ -1,27 +1,53 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
+/* eslint-disable no-console */
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+const User = require('./models/user');
+
+dotenv.config();
 
 const app = express();
 
+mongoose.connect(
+  process.env.DATABASE,
+  { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Connected to the database');
+    }
+  },
+);
 
-//MIDDLEWARE
-app.use(morgan ('dev'));
+// MIDDLEWARE
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//GET - RETRIEVE DAT FROM THE SERVER
-app.get('/', (req, res) => {
-  res.json('Hello amazon clone');
-})
+// GET - RETRIEVE DAT FROM THE SERVER
+app.post('/', (req, res) => {
+  const user = new User();
+  user.name = req.body.name;
+  user.email = req.body.email;
+  user.password = req.body.password;
+  user.save((err) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json('Successfully saved');
+    }
+  });
+});
 
-//POST - SEND DATA FROM FRONTEND TO BACK END
+// POST - SEND DATA FROM FRONTEND TO BACK END
 
 app.listen(3000, (err) => {
-  if(err){
-    console.log(err)
+  if (err) {
+    console.log(err);
   } else {
-    console.log('Listen on Port 3000')
+    console.log('Listen on Port 3000');
   }
-})
+});
