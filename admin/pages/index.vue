caf19cfef2c2ba4806067ee20f49a18b3049fab3
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-03-03 22:08:13 +0100

    Created product page for admin

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,73 +1,88 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        admin
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <main>
+    <div class="a-spacing-large"></div>
+
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div class="col-sm-8 col-8">
+          <h1 class="a-size-large a-spacing-none a-text-normal">All products</h1>
+          <div class="a-spacing-large"></div>
+
+          <!-- button -->
+          <a href="#" class="a-button-buy-again">Add a new product</a>
+          <a href="#" class="a-button-history margin-right-10">Add a new category</a>
+          <a href="#" class="a-button-history margin-right-10">Add a new owner</a>
+
+          <!-- listing page-->
+        </div>
       </div>
     </div>
-  </div>
+
+    <div class="a-spacing-large"></div>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div v-for="(product, index) in products" :key="product._id" class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+          <div class="history-box">
+            <!-- proudct image -->
+            <a href="#" class="a-link-normal">
+              <img :src="product.photo" :alt="product.title" class="img-fluid">
+            </a>
+
+            <!-- proudct title -->
+            <div class="a-spacing-top-base asin-title">
+              <span class="a-text-normal">
+                <div class="p13n-sc-truncated">{{ product.title }}</div>
+              </span>
+            </div>
+
+            <!-- product rating -->
+            <div class="a-row">
+              <a href="#">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span class="a-letter-space"></span>
+                <span class="a-color-tertiary a-size-small asin-reviews">(1732)</span>
+              </a>
+            </div>
+
+            <!-- product price -->
+            <div class="a-row">
+              <span class="a-size-base a-color-price">
+                <span class="p13n-sc-price">${{ product.price }}</span>
+              </span>
+            </div>
+
+            <!-- product button -->
+            <div class="a-row">
+              <a href="#" class="a-button-history margin-right-10">Update</a>
+              <a href="#" class="a-button-history margin-right-10">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
-export default {}
+export default {
+  // asyncData is fetching data before nuxt page finish loading on the browser
+  // it's good for SEO because the data will be loaded first
+  async asyncData({ $axios }){
+    try {
+      let response = await $axios.$get("http://localhost:3000/api/products");
+      return {
+        products: response.products
+      }
+    } catch (err) {
+      
+    }
+  }
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
