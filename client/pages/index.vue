d20ca05a85ddaf30c08cf811868fa6534b1eb1a5
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-03-04 23:49:41 +0100

    Added featuredProduct component

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -6,14 +6,22 @@
         <div class="col-xl-2 col-lg-3 md-4 col-sm-4" />
 
         <!-- main content -->
-        <div class="col-xl-10 col-lg-9 md-8 col-sm-8" />
+        <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+          <FeaturedProduct />
+        </div>
       </div>
     </div>
   </main>
 </template>
 
 <script>
-export default {}
+import FeaturedProduct from '~/components/FeaturedProduct'
+
+export default {
+  components: {
+    FeaturedProduct
+  }
+}
 </script>
 
 <style>
