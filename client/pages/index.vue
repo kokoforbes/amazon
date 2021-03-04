049f5d0d2dedec249ffe09673f16aa29fc70bc6b
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-03-04 17:13:00 +0100

    initial set up for client app

---
+++ client/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <nuxt-link to="/proudcts" class="a-button-buy-again">
+      Buy Porduct
+    </nuxt-link>
+    <div>
+      <Logo />
+      <h1 class="title">
+        client
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
