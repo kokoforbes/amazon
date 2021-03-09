62d1dea023fad52e20e7d262d31a4ef25cbee906
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-03-09 20:22:03 +0100

    Implemented signup action for client app

--- client/pages/signup.vue
+++ client/pages/signup.vue
@@ -20,19 +20,19 @@
                 <!-- name -->
                 <div class="a-row a-spacing-base">
                   <label for="ap_customer_name" class="a-form-label">Your name</label>
-                  <input id="ap_customer_name" type="text" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
+                  <input id="ap_customer_name" v-model="name" type="text" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
                 </div>
 
                 <!-- Email -->
                 <div class="a-row a-spacing-base">
-                  <label for="ap_customer_name" class="a-form-label">Email</label>
-                  <input id="ap_customer_name" type="email" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
+                  <label for="ap_customer_email" class="a-form-label">Email</label>
+                  <input id="ap_customer_email" v-model="email" type="email" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
                 </div>
 
                 <!-- Password -->
                 <div class="a-row a-spacing-base">
-                  <label for="ap_customer_name" class="a-form-label">Password</label>
-                  <input id="ap_customer_name" type="password" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
+                  <label for="ap_customer_password" class="a-form-label">Password</label>
+                  <input id="ap_customer_password" v-model="password" type="password" class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info">
                   <div class="a-alert-container">
                     <div class="a-lert-content">
                       Password must be atleast 6 characters.
@@ -44,7 +44,7 @@
                 <div class="a-row a-spacing-extra-large mb-4">
                   <span class="a-button-primary">
                     <span class="a-button-inner">
-                      <span class="a-button-text">Create your Amazon account</span>
+                      <span class="a-button-text" @click="onSignup">Create your Amazon account</span>
                     </span>
                   </span>
 
@@ -73,6 +73,42 @@
 </template>
 <script>
 export default {
-  layout: 'none'
+  layout: 'none',
+  data () {
+    return {
+      name: '',
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    async onSignup () {
+      try {
+        const data = {
+          name: this.name,
+          email: this.email,
+          password: this.password
+        }
+
+        const response = await this.$axios.$post('/api/auth/signup', data)
+
+        console.log(response)
+
+        if (response.success) {
+          this.$auth.loginWith('local', {
+            data: {
+              email: this.email,
+              password: this.password
+            }
+          })
+
+          this.$router.push('/')
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+  }
 }
 </script>
