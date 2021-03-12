ea4b1376e42b46a18386d152ef6a7d9777558f1e
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-03-12 17:38:26 +0100

    FIX: address issue

--- client/pages/address/add.vue
+++ client/pages/address/add.vue
@@ -208,8 +208,7 @@ export default {
         const data = {
           country: this.country,
           fullName: this.fullName,
-          streetAddress1: this.streetAddress1,
-          streetAddress2: this.streetAddress2,
+          streetAddress: this.streetAddress1 + '' + this.streetAddress2,
           city: this.city,
           state: this.state,
           zipCode: this.zipCode,
