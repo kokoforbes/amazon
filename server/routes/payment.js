3f4f7298909d462cf03f021cf565b857d61742b8
Onikoko Abdullahi <onikokoabdullahi@gmail.com>
2021-03-17 14:03:57 +0100

    Created POST method for payment API

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 const moment = require('moment');
+const stripe = require('stripe');
+const verifyToken = require('../middlewares/verify-token');
+const Order = require('../models/order');
 
 const SHIPMENT = {
   normal: {
@@ -32,6 +35,41 @@ router.post('/shipment', (req, res) => {
     success: true,
     shipment,
   });
+});
+
+router.post('/payment', verifyToken, (req, res) => {
+  const totalPrice = Math.round(req.body.totalPrice * 100);
+
+  stripe.customers.create({
+    email: req.decoded.email,
+  }).then((customer) => stripe.customers.createSource(customer.id, {
+    source: 'tok_visa',
+  })).then((source) => stripe.charges.create({
+    amount: totalPrice,
+    currency: 'usd',
+    customer: source.customer,
+  })).then(async (charge) => {
+    const order = new Order();
+    const { cart } = req.body;
+
+    cart.map((product) => {
+      order.products.push({
+        productID: product._id,
+        quantity: parseInt(product.quantity),
+        price: product.price,
+      });
+    });
+
+    order.owner = req.decoded._id;
+    order.estimatedDelivery = req.body.estimatedDelivery;
+    await order.save();
+  })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    });
 });
 
 module.exports = router;
